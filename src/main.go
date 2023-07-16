b2baa7181b4d79164eba4d75cd9a319d78b166dd
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-07-16 21:57:22 +0900

    perlを実行可能に実装

--- src/main.go
+++ src/main.go
@@ -1,29 +1,109 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
+	"regexp"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
-func main() {
-	// インスタンスを作成
-	e := echo.New()
-
-	// ミドルウェアを設定
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// ルートを設定
-	e.GET("/", hello)
-
-	// サーバーをポート番号1323で起動
-	e.Logger.Fatal(e.Start(":" + os.Getenv("APP_PORT")))
+type CodeRequest struct {
+	Code string `json:"code"`
 }
 
-// ハンドラーを定義
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type ExecutionResult struct {
+	Output string `json:"output"`
+	Error  string `json:"error,omitempty"`
+}
+
+func main() {
+	http.HandleFunc("/execute", handleExecute)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleExecute(w http.ResponseWriter, r *http.Request) {
+	// リクエストボディの読み取り
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// JSONデコード
+	var req CodeRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Perlスクリプトの実行
+	// cmd := exec.Command("perl", "-e", req.Code)
+	// output, err := cmd.Output()
+
+	ctx := context.Background()
+
+	// Dockerクライアントの作成
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	// コンテナ名
+	containerName := "go-play-langs-perl"
+
+	// コンテナでコマンドを実行
+	execResp, err := cli.ContainerExecCreate(ctx, containerName, types.ExecConfig{
+		Cmd:          []string{"perl", "-e", req.Code},
+		AttachStdout: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// コンテナ実行結果の読み取り
+	execAttachResp, err := cli.ContainerExecAttach(ctx, execResp.ID, types.ExecStartCheck{})
+	if err != nil {
+		panic(err)
+	}
+	defer execAttachResp.Close()
+
+	// 実行結果の読み込み
+	outputBytes, err := ioutil.ReadAll(execAttachResp.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	// 実行結果の整形
+	output := string(outputBytes)
+	// 制御文字や不可視文字を削除する
+	output = removeNonPrintableChars(output)
+
+	// 実行結果をJSON形式で返す
+	result := ExecutionResult{}
+	if err != nil {
+		result.Error = err.Error()
+	} else {
+		result.Output = output
+	}
+
+	response, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
+// 制御文字や不可視文字を削除する関数
+func removeNonPrintableChars(s string) string {
+	reg := regexp.MustCompile("[[:cntrl:]]")
+	return reg.ReplaceAllString(s, "")
 }
